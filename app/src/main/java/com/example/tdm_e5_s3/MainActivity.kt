06ee81c99ce52a70e71c76035a002f4482b76d0b
kherroubioussama <ho_kherroubi@esi.dz>
2021-05-26 00:02:10 +0100

    first commit

---
+++ app/src/main/java/com/example/tdm_e5_s3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tdm_e5_s3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
